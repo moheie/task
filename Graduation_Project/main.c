7fa959f156e746f65a7d58b020b03c13d2d7a843
moheie <109584504+moheie@users.noreply.github.com>
2023-02-11 11:22:02 +0200

    first commit

---
+++ Graduation_Project/main.c
@@ -0,0 +1,46 @@
+// All the preprocessor Libaries and Macros
+#define  F_CPU 16000000
+#include <util/delay.h>
+
+#include "LED.h"
+
+#include "Timer.h"
+
+extern Uint8 x;
+extern Uint8 y;
+extern Uint8 z;
+
+
+int main(void)
+{	
+
+    
+	LED0_Init();
+	LED1_Init();
+	LED2_Init();
+	
+	Timer0_Init();
+	Timer0_Start();
+	Timer0_SetDelay(5);
+		
+	while (1)
+	
+	{		
+		
+		if(x==1)
+		{
+			LED0_Toggle();
+			x=0;
+		}
+		if(y==1)
+		{
+			LED1_Toggle();
+			y=0;
+		}
+		if(z==1)
+		{
+			LED2_Toggle();
+			z=0;
+		}
+	}
+}
